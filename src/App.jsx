ee1119884bf518939982bc230a3f87db4be6eaa6
Sanzana <sanzanamahmud1@gmail.com>
2026-09-12 16:38:19 +0600

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './component/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div className='w-full h-8'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import img from "../../assets/logo-text.png"
+import hamburgerIcon from "../../assets/hamburger.png"
+
+function Navbar() {
+    const[isOpen, setIsOpen] = useState(false);
+  return (
+    <div className='w-full h-24 p-10 flex justify-between items-center sticky top-0 z-50'>
+        <div className='md:hidden flex items-center'>
+            <button onClick={() => setIsOpen(!isOpen)} className='focus:outline-none'>
+                <img src={hamburgerIcon} alt="" className="h-8 w-8 object-contain"/>
+            </button>
+        </div>
+        <div className='flex items-center max-md:absolute max-md:left-1/2 max-md:-translate-x-1/2'>
+            <img src={img} alt="" className="h-8 w-auto object-contain"/>
+        </div>
+
+            <ul className='hidden md:flex flex-row gap-5 list-none items-center'>
+                <li className='text-[#DB2777]'><a href="#">Home</a></li>
+                <li><a href="#">Technologies</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+
+        <div className='flex items-center gap-4'>
+            <button className='text-[#475569 whitespace-nowrap]'>Sign In</button>
+            <button className='bg-[#DB2777] px-5 py-2 rounded-full text-white'>Sign Up</button>
+        </div>
+
+        {isOpen && (
+            <div className='absolute top-24 left-0 w-48 text-left shadow-md md:hidden'>
+            <ul className='flex flex-col gap-5 px-6 py-5 items-start list-none'>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Technologies</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        )}
+    </div>
+  )
+}
+
+export default Navbar
